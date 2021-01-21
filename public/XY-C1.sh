b2254c80133007f3a5087a2f6b6fc51aa9064360
kinkitchen <38160775+kinkitchen@users.noreply.github.com>
2021-01-21 16:50:55 +0800

    Update and rename PSG1218.sh to XY-C1.sh

--- public/XY-C1.sh
+++ public/XY-C1.sh
@@ -11,10 +11,10 @@ config="./configs/templates/PSG1218.config" # 默认配置文件
 echo "修改 LAN IP 地址"
 sed -i "s/192.168.2/$lan_ip/g" $default_path/defaults.h
 
-# echo "设置为PPPOE模式并写入账号和密码"
-# sed -i 's/{ "wan_proto", "dhcp" }/{ "wan_proto", "pppoe" }/g' $default_path/defaults.c
-# sed -i 's/{ "wan_pppoe_username", "" }/{ "wan_pppoe_username", "你的宽带账号" }/g' $default_path/defaults.c
-# sed -i 's/{ "wan_pppoe_passwd", "" }/{ "wan_pppoe_passwd", "宽带密码" }/g' $default_path/defaults.c
+ echo "设置为PPPOE模式并写入账号和密码"
+ sed -i 's/{ "wan_proto", "dhcp" }/{ "wan_proto", "pppoe" }/g' $default_path/defaults.c
+ sed -i 's/{ "wan_pppoe_username", "" }/{ "wan_pppoe_username", "13751663678@139.gd" }/g' $default_path/defaults.c
+ sed -i 's/{ "wan_pppoe_passwd", "" }/{ "wan_pppoe_passwd", "201809" }/g' $default_path/defaults.c
 
 # echo "设置为中继模式"
 # echo "#define BOARD_ROLE_REPEATER	1" >> ./configs/boards/PSG1218/board.h
